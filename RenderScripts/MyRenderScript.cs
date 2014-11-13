423e17de9954d993c9c2424574a155964b836383
Zach Saw <zach.saw@gmail.com>
2014-11-13 18:11:20 +1100

    Added Nedi quadrupling in MyRenderScript.cs example. Fixed IsDownscalingFrom/IsNotScalingFrom/IsUpscalingFrom methods.

--- RenderScripts/MyRenderScript.cs
+++ RenderScripts/MyRenderScript.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 using Mpdn.RenderScript.Mpdn.ImageProcessor;
 using Mpdn.RenderScript.Mpdn.Resizer;
 using Mpdn.RenderScript.Shiandow.Nedi;
@@ -18,12 +21,12 @@ namespace Mpdn.RenderScript
             {
                 return new[]
                 {
-                    Nedi = NediScaler.Create(),
+                    Nedi = NediScaler.Create(forced: true),
                     PreProcess = ImageProcessor.Create(PreResizeShaderfiles),
                     PostProcess = ImageProcessor.Create(PostResizeShaderfiles),
                     ToLinear = ImageProcessor.Create(new[] {@"ConvertToLinearLight.hlsl"}),
                     ToGamma = ImageProcessor.Create(new[] {@"ConvertToGammaLight.hlsl"}),
-                    ResizeToTarget = Resizer.Create(),
+                    ResizeToTarget = Resizer.Create(option: ResizerOption.TargetSize100Percent),
                     // Add more scripts here ...
                 };
             }
@@ -34,9 +37,21 @@ namespace Mpdn.RenderScript
 
                 // Pre resize shaders, followed by NEDI image doubler
                 result.Add(PreProcess);
-                result.Add(Nedi);
 
-                if (IsDownscalingFrom(Nedi)) // See RenderScriptChain for other comparer methods
+                var size = Renderer.VideoSize;
+                if (IsUpscalingFrom(size))
+                {
+                    result.Add(Nedi);
+                    size = DoubleSize(size);
+                    if (IsUpscalingFrom(size))
+                    {
+                        // Quadruple image using Nedi if required
+                        result.Add(Nedi);
+                        size = DoubleSize(size);
+                    }
+                }
+
+                if (IsDownscalingFrom(size)) // See RenderScriptChain for other comparer methods
                 {
                     // For final downscaling, use linear light scaling
                     result.Add(ToLinear);
@@ -53,6 +68,11 @@ namespace Mpdn.RenderScript
                 result.Add(PostProcess);
 
                 return result.ToArray();
+            }
+
+            private static Size DoubleSize(Size size)
+            {
+                return new Size(size.Width*2, size.Height*2);
             }
         }
     }
