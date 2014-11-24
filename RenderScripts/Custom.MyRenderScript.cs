df09a8cc18c4361a436f8602612a2d45a75ea473
Zach Saw <zach.saw@gmail.com>
2014-11-24 16:40:35 +1100

    Quick reformatting for Custom.MyRenderScript.cs. Updated README.md to include a section on developing / debugging render scripts.

--- RenderScripts/Custom.MyRenderScript.cs
+++ RenderScripts/Custom.MyRenderScript.cs
@@ -8,52 +8,53 @@ namespace Mpdn.RenderScript
 {
     namespace MyOwnUniqueNameSpace // e.g. Replace with your user name
     {
-        public class MyRenderChain : CombinedChain {
-            private string[] PreProcess = { @"SweetFX\Bloom.hlsl", @"SweetFX\LiftGammaGain.hlsl" };
-            private string[] PostProcess = { @"SweetFX\LumaSharpen.hlsl" };
-            private string[] Deinterlace = { @"MPC-HC\Deinterlace (blend).hlsl" };
-            private string[] ToLinear = { @"ConvertToLinearLight.hlsl" };
-            private string[] ToGamma = { @"ConvertToGammaLight.hlsl" };
+        public class MyRenderChain : CombinedChain
+        {
+            private readonly string[] Deinterlace = {@"MPC-HC\Deinterlace (blend).hlsl"};
+            private readonly string[] PostProcess = {@"SweetFX\LumaSharpen.hlsl"};
+            private readonly string[] PreProcess = {@"SweetFX\Bloom.hlsl", @"SweetFX\LiftGammaGain.hlsl"};
+            private readonly string[] ToGamma = {@"ConvertToGammaLight.hlsl"};
+            private readonly string[] ToLinear = {@"ConvertToLinearLight.hlsl"};
 
             protected override void BuildChain(FilterChain chain)
             {
                 // Scale chroma first (this bypasses MPDN's chroma scaler)
-                chain.Add(new BicubicChroma{ Preset = Presets.MitchellNetravali });
+                chain.Add(new BicubicChroma {Preset = Presets.MitchellNetravali});
 
                 if (Renderer.InterlaceFlags.HasFlag(InterlaceFlags.IsInterlaced))
                 {
                     // Deinterlace using blend
-                    chain.Add(new ImageProcessor { ShaderFileNames = Deinterlace });
+                    chain.Add(new ImageProcessor {ShaderFileNames = Deinterlace});
                 }
 
                 // Pre resize shaders, followed by NEDI image doubler
-                chain.Add(new ImageProcessor { ShaderFileNames = PreProcess });
+                chain.Add(new ImageProcessor {ShaderFileNames = PreProcess});
 
                 // Use NEDI once only.
                 // Note: To use NEDI as many times as required to get the image past target size,
                 //       Change the following *if* to *while*
                 if (IsUpscalingFrom(chain)) // See CombinedChain for other comparer methods
                 {
-                    chain.Add(new Nedi { AlwaysDoubleImage = true });
+                    chain.Add(new Nedi {AlwaysDoubleImage = true});
                 }
 
                 if (IsDownscalingFrom(chain))
                 {
                     // Use linear light for downscaling
-                    chain.Add(new ImageProcessor { ShaderFileNames = ToLinear });
-                    chain.Add(new Resizer { ResizerOption = ResizerOption.TargetSize100Percent });
-                    chain.Add(new ImageProcessor { ShaderFileNames = ToGamma });
+                    chain.Add(new ImageProcessor {ShaderFileNames = ToLinear});
+                    chain.Add(new Resizer {ResizerOption = ResizerOption.TargetSize100Percent});
+                    chain.Add(new ImageProcessor {ShaderFileNames = ToGamma});
                 }
                 else
                 {
                     // Otherwise, scale with gamma light
-                    chain.Add(new Resizer { ResizerOption = ResizerOption.TargetSize100Percent });
+                    chain.Add(new Resizer {ResizerOption = ResizerOption.TargetSize100Percent});
                 }
 
                 if (Renderer.VideoSize.Width >= 1920)
                 {
                     // Sharpen only if video isn't full HD
-                    chain.Add(new ImageProcessor { ShaderFileNames = PostProcess });
+                    chain.Add(new ImageProcessor {ShaderFileNames = PostProcess});
                 }
             }
         }
@@ -69,7 +70,7 @@ namespace Mpdn.RenderScript
                         Name = "Custom Render Script Chain",
                         Description = "A customized render script chain (Advanced)",
                         Guid = new Guid("B0AD7BE7-A86D-4BE4-A750-4362FEF28A55"),
-                        Copyright = ""
+                        Copyright = "" // Optional field
                     };
                 }
             }
