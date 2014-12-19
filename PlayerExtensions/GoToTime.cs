3cfbc8f05cd8bb1b6fd565ebec764d80b1877b66
Zach Saw <zach.saw@gmail.com>
2014-12-19 22:45:23 +1100

    Added "Go to time" extension.

---
+++ PlayerExtensions/GoToTime.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Mpdn.PlayerExtensions.ZachSaw
+{
+    public class GoToTime : IPlayerExtension
+    {
+        private IPlayerControl m_PlayerControl;
+
+        public ExtensionDescriptor Descriptor
+        {
+            get
+            {
+                return new ExtensionDescriptor
+                {
+                    Guid = new Guid("7C3BA1E2-EE7B-47D2-B174-6AE76D65EC04"),
+                    Name = "Go To Time",
+                    Description = "Jump to a specified timecode in media",
+                    Copyright = "Copyright Zach Saw © 2014. All rights reserved."
+                };
+            }
+        }
+
+        public void Initialize(IPlayerControl playerControl)
+        {
+            m_PlayerControl = playerControl;
+            m_PlayerControl.KeyDown += PlayerKeyDown;
+        }
+
+        public void Destroy()
+        {
+            m_PlayerControl.KeyDown -= PlayerKeyDown;
+        }
+
+        public IList<Verb> Verbs
+        {
+            get
+            {
+                return new[]
+                {
+                    new Verb(Category.Play, string.Empty, "Go To...", "Ctrl+G", string.Empty, GotoPosition)
+                };
+            }
+        }
+
+        private void GotoPosition()
+        {
+            using (var form = new GoToTimeForm())
+            {
+                if (form.ShowDialog(m_PlayerControl.Form) != DialogResult.OK)
+                    return;
+
+                if (m_PlayerControl.PlayerState == PlayerState.Closed)
+                    return;
+
+                if (m_PlayerControl.PlayerState == PlayerState.Stopped)
+                {
+                    m_PlayerControl.PauseMedia(false);
+                }
+
+                m_PlayerControl.SeekMedia(form.Position * 1000);
+            }
+        }
+
+        private void PlayerKeyDown(object sender, PlayerKeyEventArgs e)
+        {
+            switch (e.Key.KeyData)
+            {
+                case Keys.Control | Keys.G:
+                    GotoPosition();
+                    break;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-PlayerExtensions
-================
-
 MPDN Player Extensions
+======================
+
+MPDN project page - http://forum.doom9.org/showthread.php?t=171120
+
+<H3>Developers</H3>
+<ul>
+<li>Zachs</li>
+</ul>
+
+
+How to use Player Extensions?
+-----------------------------
+
+To use these player extensions (compatible with ***MPDN v2.15.0*** and above), click the **Download ZIP** button on the right to download the whole repository.
+
+Then extract the files and folders in the PlayerExtensions folder (located in PlayerExtensions-master folder) to your MPDN's PlayerExtensions folder.
+
+
+Developing / Debugging Player Extensions
+----------------------------------------
+
+The easiest way to develop or debug player extensions is to use Microsoft Visual Studio or similar IDEs.
+
+Follow these simple steps:<ol><li>Create a class library</li><li>Add the following assembly references to your project:<ul><li>`Mpdn.Definitions.dll`</li></li></ul></li><li>Set your class library's output folder to MPDN's PlayerExtensions folder</li><li>Set the program to debug your class library to MediaPlayerDotNet.exe</li><li>You're all set! This allows your IDE to run MPDN which in turn loads your class library (PlayerExtension plugin) when you start a debug session</li></ol>
+
+You can set breakpoints and step through your code just as you normally would. Intellisense should work too.
