875748807d419f2166b180b33a3178a63acbf886
Shiandow <Shiandow@gmail.com>
2015-01-06 14:05:26 +0100

    Changes to SuperRes

--- RenderScripts/Shiandow.SuperRes.cs
+++ RenderScripts/Shiandow.SuperRes.cs
@@ -4,6 +4,7 @@ using Mpdn.RenderScript.Scaler;
 using SharpDX;
 using YAXLib;
 using Mpdn.RenderScript.Shiandow.Nedi;
+using Mpdn.CustomLinearScaler.Example;
 
 namespace Mpdn.RenderScript
 {
@@ -43,13 +44,15 @@ namespace Mpdn.RenderScript
                 Passes = 2;
 
                 Strength = 0.8f;
-                Sharpness = 0.5f;
-                AntiAliasing = 1.0f;
+                Sharpness = 0.85f;
+                AntiAliasing = 0.65f;
                 AntiRinging = 0.8f;
 
                 UseNEDI = false;
                 FirstPassOnly = false;
-                upscaler = new Scaler.Bicubic(2.0f/3.0f, false);
+                //upscaler = new Scaler.Bicubic(2.0f/3.0f, false);
+                //upscaler = new Scaler.Custom(new Gaussian(), ScalerTaps.Four, false);
+                upscaler = new Scaler.Jinc(ScalerTaps.Four, false);
                 downscaler = new Scaler.Bilinear();
             }
 
@@ -58,15 +61,15 @@ namespace Mpdn.RenderScript
                 var inputSize = sourceFilter.OutputSize;
                 var targetSize = TargetSize();
 
-                IFilter initial;
+                IResizeableFilter initial;
                 if (UseNEDI)
                 {
-                    initial = new Shiandow.Nedi.Nedi { AlwaysDoubleImage = false, Centered = false }.CreateFilter(sourceFilter);
-                    initial = new ResizeFilter(initial, targetSize, m_ShiftedScaler, m_ShiftedScaler);
+                    var nedi = new Shiandow.Nedi.Nedi { AlwaysDoubleImage = false, Centered = false }.CreateFilter(sourceFilter);
+                    initial = new ResizeFilter(nedi, targetSize, m_ShiftedScaler, m_ShiftedScaler);
                 }
                 else
                 {
-                    initial = new ResizeFilter(sourceFilter, targetSize);
+                    initial = new ResizeFilter(sourceFilter, inputSize); // SetSize must *not* affect original. 
                 }
 
                 // Skip if downscaling
@@ -76,11 +79,12 @@ namespace Mpdn.RenderScript
                     return CreateFilter(sourceFilter, initial);
             }
 
-            public IFilter CreateFilter(IFilter original, IFilter initial)
+            public IFilter CreateFilter(IFilter original, IResizeableFilter initial)
             {
-                IFilter lab, linear;
+                IFilter lab, linear, result = initial;
 
                 var inputSize = original.OutputSize;
+                var currentSize = initial.OutputSize;
                 var targetSize = TargetSize();
 
                 var Diff = CompileShader("Diff.hlsl");
@@ -95,29 +99,55 @@ namespace Mpdn.RenderScript
                 var LinearToLab = CompileShader("LinearToLab.hlsl");
 
                 // Initial scaling
-                linear   = new ShaderFilter(GammaToLinear, initial);
+                lab = new ShaderFilter(GammaToLab, initial);
                 original = new ShaderFilter(GammaToLab, original);
 
                 float AA = AntiAliasing;
                 for (int i = 1; i <= Passes; i++)
                 {
                     IFilter res, diff;
+
+                    // Initial calculations
                     bool useBilinear = (upscaler is Scaler.Bilinear) || (FirstPassOnly && !(i == 1));
+                    /*
+                    if (i == Passes) currentSize = targetSize;
+                    else currentSize = CalculateSize(currentSize, targetSize, i);
+                     */
+                    currentSize = targetSize;
+                    
+                    // Resize and Convert
+                    lab = new ResizeFilter(lab, currentSize);
+                    linear = new ShaderFilter(LabToLinear, lab);
 
                     // Calculate difference
                     res = new ResizeFilter(linear, inputSize, upscaler, downscaler); // Downscale result
                     diff = new ShaderFilter(Diff, res, original);                    // Compare with original
                     if (!useBilinear)
-                        diff = new ResizeFilter(diff, targetSize, upscaler, downscaler); // Scale to output size
-
+                        diff = new ResizeFilter(diff, currentSize, upscaler, downscaler); // Scale to output size
+                    
                     // Update result
-                    lab = new ShaderFilter(LinearToLab, linear);
-                    linear = new ShaderFilter(SuperRes, useBilinear, new[] { Strength, Sharpness, AA, AntiRinging }, lab, diff, original);
+                    lab = new ShaderFilter(SuperRes, useBilinear, new[] { Strength, Sharpness, AA, AntiRinging }, lab, diff, original);
+                    result = new ShaderFilter(LabToGamma, lab);
 
-                    AA *= 0.5f;
+                    AA *= 0.65f;
                 }
 
-                return new ShaderFilter(LinearToGamma, linear);
+                return result;
+            }
+
+            private Size CalculateSize(Size sizeA, Size sizeB, int k)
+            {
+                int minW = sizeA.Width; int minH = sizeA.Height;
+                int maxW = sizeB.Width; int maxH = sizeB.Height;
+
+                int steps = (int)Math.Floor(Math.Log((double)(maxH * maxW) / (double)(minH * minW)) / (2 * Math.Log(1.5)));
+                if (steps < 1) steps = 1;
+
+                double w = minW * Math.Pow((double)maxW / (double)minW, (double)Math.Min(k, steps) / (double)steps);
+                double h = minW * Math.Pow((double)maxH / (double)minH, (double)Math.Min(k, steps) / (double)steps);
+
+                return new Size(Math.Min(maxW, Math.Max(minW, (int)Math.Round(w))),
+                                Math.Min(maxH, Math.Max(minH, (int)Math.Round(h))));
             }
 
             private IScaler m_ShiftedScaler;
--- RenderScripts/SuperRes/Diff.hlsl
+++ RenderScripts/SuperRes/Diff.hlsl
@@ -23,13 +23,11 @@ float2 p1 :  register(c1);
 #define false 6
 
 // -- Gamma processing --
-#define RGBtoXYZ float3x3(float3(0.4124,0.3576,0.1805),float3(0.2126,0.7152,0.0722),float3(0.0193,0.1192,0.9502))
-#define XYZtoRGB (625.0*float3x3(float3(67097680, -31827592, -10327488), float3(-20061906, 38837883, 859902), float3(1153856, -4225640, 21892272))/12940760409.0)
 #define A (0.272433)
 
 #if GammaCurve == sRGB
-float3 Gamma(float3 x)   { return lerp(x * 12.9232102, 1.055*pow(x, 1 / 2.4) - 0.055, saturate(0.5 + 65536 * (x - (0.0392857 / 12.9232102)))); }
-float3 GammaInv(float3 x){ return lerp(x / 12.9232102, pow((x + 0.055) / 1.055, 2.4), saturate(0.5 + 65536 * (x - 0.0392857))); }
+float3 Gamma(float3 x)   { return x < (0.0392857 / 12.9232102) ? x * 12.9232102 : 1.055*pow(x, 1 / 2.4) - 0.055; }
+float3 GammaInv(float3 x){ return x <  0.0392857			   ? x / 12.9232102 : pow((x + 0.055) / 1.055, 2.4); }
 #elif GammaCurve == Power
 float3 Gamma(float3 x)   { return pow(saturate(x), 1 / gamma); }
 float3 GammaInv(float3 x){ return pow(saturate(x), gamma); }
@@ -41,19 +39,44 @@ float3 Gamma(float3 x)   { return x; }
 float3 GammaInv(float3 x){ return x; }
 #endif
 
-// -- Color space Processing --
-float3 Labf(float3 x)   { return lerp(x * (29 * 29) / (3 * 6 * 6) + (4 / 29), pow(x, 1 / 3.0), saturate(0.5 + 65536 * (x - (6 * 6 * 6 / (29 * 29 * 29))))); }
-float3 Labfinv(float3 x){ return lerp((x - 4 / 29) * (3 * 6 * 6) / (29 * 29), x*x*x, saturate(0.5 + 65536 * (x - (6 / 29)))); }
+// -- Colour space Processing --
+#define Kb 0.114
+#define Kr 0.299
+#define RGBtoYUV float3x3(float3(Kr, 1 - Kr - Kb, Kb), float3(-Kr, Kr + Kb - 1, 1 - Kb) / (2*(1 - Kb)), float3(1 - Kr, Kr + Kb - 1, -Kb) / (2*(1 - Kr)))
+#define YUVtoRGB float3x3(float3(1, 0, 2*(1 - Kr)), float3(Kb + Kr - 1, 2*(1 - Kb)*Kb, 2*Kr*(1 - Kr)) / (Kb + Kr - 1), float3(1, 2*(1 - Kb),0))
+#define RGBtoXYZ float3x3(float3(0.4124,0.3576,0.1805),float3(0.2126,0.7152,0.0722),float3(0.0193,0.1192,0.9502))
+#define XYZtoRGB (625.0*float3x3(float3(67097680, -31827592, -10327488), float3(-20061906, 38837883, 859902), float3(1153856, -4225640, 21892272))/12940760409.0)
+#define YUVtoXYZ mul(RGBtoXYZ,YUVtoRGB)
+#define XYZtoYUV mul(RGBtoYUV,XYZtoRGB)
 
-float3 RGBToLab(float3 rgb) {
+float3 Labf(float3 x)   { return x < (6.0*6.0*6.0) / (29.0*29.0*29.0) ? (x * (29.0 * 29.0) / (3.0 * 6.0 * 6.0)) + (4.0 / 29.0) : pow(x, 1.0 / 3.0); }
+float3 Labfinv(float3 x){ return x < (6.0 / 29.0)					  ? (x - (4.0 / 29.0)) * (3.0 * 6.0 * 6.0) / (29.0 * 29.0) : x*x*x; }
+
+float3 DLabf(float3 x)   { return min((29.0 * 29.0) / (3.0 * 6.0 * 6.0), (1.0/3.0) / pow(x, (2.0 / 3.0))); }
+float3 DLabfinv(float3 x){ return max((3.0 * 6.0 * 6.0) / (29.0 * 29.0), 3.0*x*x); }
+float3 RGBtoLab(float3 rgb) {
 	float3 xyz = mul(RGBtoXYZ, rgb);
 	xyz = Labf(xyz);
 	return float3(1.16*xyz.y - 0.16, 5.0*(xyz.x - xyz.y), 2.0*(xyz.y - xyz.z));
 }
 
-float3 LabToRGB(float3 res) {
-	float3 xyz = (res.x + 0.16) / 1.16 + float3(res.y / 5.0, 0, -res.z / 2.0);
+float3 LabtoRGB(float3 lab) {
+	float3 xyz = (lab.x + 0.16) / 1.16 + float3(lab.y / 5.0, 0, -lab.z / 2.0);
 	return saturate(mul(XYZtoRGB, Labfinv(xyz)));
+}
+
+float3x3 DRGBtoLab(float3 rgb) {
+	float3 xyz = mul(RGBtoXYZ, rgb);
+	xyz = DLabf(xyz);
+	float3x3 D = { { xyz.x, 0, 0 }, { 0, xyz.y, 0 }, { 0, 0, xyz.z } };
+	return mul(D, RGBtoXYZ);
+}
+
+float3x3 DLabtoRGB(float3 lab) {
+	float3 xyz = (lab.x + 0.16) / 1.16 + float3(lab.y / 5.0, 0, -lab.z / 2.0);
+	xyz = DLabfinv(xyz);
+	float3x3 D = { { xyz.x, 0, 0 }, { 0, xyz.y, 0 }, { 0, 0, xyz.z } };
+	return mul(XYZtoRGB, D);
 }
 
 // -- Main code --
@@ -61,7 +84,8 @@ float4 main(float2 tex : TEXCOORD0) : COLOR {
 	float4 c0 = tex2D(s0, tex);
 	float4 c1 = tex2D(s1, tex);
 
-	c0.xyz = RGBToLab(c0.rgb);
+	c0.xyz = RGBtoLab(c0.rgb);
 
-	return c0 - c1;
+	return float4(mul(DLabtoRGB(c0.xyz), c0.xyz - c1.xyz),0);
+	//return float4(c0.xyz - c1.xyz, 0);
 }
