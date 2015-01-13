38a862761fc87d471becb900981af63ef9dab79e
Zach Saw <zach.saw@gmail.com>
2015-01-13 15:50:43 +1100

    Full fledged automatic display refresh rate changer

--- PlayerExtensions/DisplayChanger.cs
+++ PlayerExtensions/DisplayChanger.cs
@@ -1,13 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using DirectShowLib;
 using Mpdn.PlayerExtensions.Example.DisplayChangerNativeMethods;
 
 namespace Mpdn.PlayerExtensions.Example
 {
     public class DisplayChanger : ConfigurablePlayerExtension<DisplayChangerSettings, DisplayChangerConfigDialog>
     {
+        private Screen m_RestoreScreen;
+        private int m_RestoreFrequency;
+
         protected override PlayerExtensionDescriptor ScriptDescriptor
         {
             get
@@ -16,7 +22,7 @@ namespace Mpdn.PlayerExtensions.Example
                 {
                     Guid = new Guid("9C1BBA5B-B956-43E1-9A91-58B72571EF82"),
                     Name = "Display Changer",
-                    Description = "Changes display based on video",
+                    Description = "Changes display refresh rate based on video",
                     Copyright = "Copyright Example © 2015. All rights reserved."
                 };
             }
@@ -32,6 +38,7 @@ namespace Mpdn.PlayerExtensions.Example
             base.Initialize(playerControl);
 
             PlayerControl.PlayerStateChanged += PlayerStateChanged;
+            PlayerControl.FormClosed += FormClosed;
         }
 
         public override void Destroy()
@@ -39,6 +46,7 @@ namespace Mpdn.PlayerExtensions.Example
             base.Destroy();
 
             PlayerControl.PlayerStateChanged -= PlayerStateChanged;
+            PlayerControl.FormClosed -= FormClosed;
         }
 
         public override IList<Verb> Verbs
@@ -46,20 +54,106 @@ namespace Mpdn.PlayerExtensions.Example
             get { return new Verb[0]; }
         }
 
+        private void FormClosed(object sender, EventArgs eventArgs)
+        {
+            RestoreSettings();
+        }
+
         private void PlayerStateChanged(object sender, PlayerStateEventArgs e)
         {
-            if (!Settings.Activate)
+            if (e.NewState == PlayerState.Closed)
+            {
+                RestoreSettings();
                 return;
+            }
 
             if (e.OldState != PlayerState.Closed)
                 return;
 
+            if (!Activated())
+                return;
+
+            m_RestoreFrequency = 0;
+
             var screen = Screen.FromControl(PlayerControl.Form);
 
-            int frequency = 0;
-            int index = 0;
+            var frequencies = GetFrequencies(screen);
 
-            // loop over all supported settings
+            if (!frequencies.Any())
+                return;
+
+            var fps = 1000000 / PlayerControl.VideoInfo.AvgTimePerFrame;
+
+            // Find the highest frequency that matches fps
+            var frequency = frequencies.OrderByDescending(f => f).FirstOrDefault(f => f % (int)Math.Round(fps) == 0);
+            if (frequency == 0)
+                return;
+            
+            ChangeRefreshRate(screen, frequency);
+        }
+
+        private bool Activated()
+        {
+            if (!Settings.Activate)
+                return false;
+
+            if (!Settings.Restricted) 
+                return true;
+
+            // Parse video types
+            var videoTypes = Settings.VideoTypes.ToLowerInvariant().Split(' ');
+            return videoTypes.Any(VideoTypeMatches);
+        }
+
+        private bool VideoTypeMatches(string vt)
+        {
+            var regexValidate = new Regex(@"^(w{1}\d+)?(h{1}\d+)?((i|p){1}\d+)?$");
+            if (regexValidate.Matches(vt).Count == 0)
+                return false;
+
+            var vid = PlayerControl.VideoInfo.BmiHeader;
+
+            var regexWidth = new Regex(@"w(\d+)");
+            var widthMatch = regexWidth.Match(vt).Groups[1];
+            var width = widthMatch.Success ? int.Parse(widthMatch.Value) : vid.Width;
+
+            var regexHeight = new Regex(@"h(\d+)");
+            var heightMatch = regexHeight.Match(vt).Groups[1];
+            var height = heightMatch.Success ? int.Parse(heightMatch.Value) : vid.Height;
+
+            var vidIsInterlaced = PlayerControl.VideoInfo.InterlaceFlags.HasFlag(AMInterlace.IsInterlaced);
+            var vidFps = 1000000/(int) PlayerControl.VideoInfo.AvgTimePerFrame;
+
+            bool interlaced = vidIsInterlaced;
+            int frameRate = vidFps;
+
+            var regexFrameRate = new Regex(@"(i|p)(\d+)");
+            var frameRateMatch = regexFrameRate.Match(vt);
+            if (frameRateMatch.Success)
+            {
+                interlaced = frameRateMatch.Groups[1].Value == "i";
+                frameRate = int.Parse(frameRateMatch.Groups[2].Value);
+            }
+
+            return width == vid.Width && height == vid.Height && interlaced == vidIsInterlaced && frameRate == vidFps;
+        }
+
+        private void RestoreSettings()
+        {
+            if (m_RestoreFrequency == 0)
+                return;
+
+            if (Settings.Restore)
+            {
+                ChangeRefreshRate(m_RestoreScreen, m_RestoreFrequency);
+            }
+            m_RestoreFrequency = 0;
+        }
+
+        private static IList<int> GetFrequencies(Screen screen)
+        {
+            var frequencies = new List<int>();
+            int index = 0;
             while (true)
             {
                 var dm = NativeMethods.CreateDevmode(screen.DeviceName);
@@ -78,46 +172,66 @@ namespace Mpdn.PlayerExtensions.Example
                 if (dm.dmPelsHeight != screen.Bounds.Height)
                     continue;
 
-                var fps = 1000000 / (int) PlayerControl.VideoInfo.AvgTimePerFrame;
-                if (fps <= 30) // We use the higher refresh rates
-                {
-                    fps *= 2;
-                }
-
-                if (dm.dmDisplayFrequency != fps) 
-                    continue;
-
-                frequency = fps;
-                break;
+                frequencies.Add(dm.dmDisplayFrequency);
             }
 
-            if (frequency != 0)
-            {
-                ChangeRefreshRate(screen, frequency);
-            }
+            return frequencies;
         }
 
         private void ChangeRefreshRate(Screen screen, int frequency)
         {
+            bool wasFullScreen = false;
+            if (PlayerControl.InFullScreenMode)
+            {
+                wasFullScreen = true;
+                // We can't change frequency in exclusive mode
+                PlayerControl.GoWindowed();
+            }
+
             var dm = NativeMethods.CreateDevmode(screen.DeviceName);
             if (GetSettings(ref dm, screen.DeviceName) == 0)
                 return;
 
+            if (dm.dmDisplayFrequency == frequency)
+                return;
+
+            var oldFreq = dm.dmDisplayFrequency;
+            m_RestoreScreen = screen;
+
             dm.dmFields = (int) DM.DisplayFrequency;
             dm.dmDisplayFrequency = frequency;
             dm.dmDeviceName = screen.DeviceName;
-            PlayerControl.StopMedia();
-            ChangeSettings(dm);
-            PlayerControl.PlayMedia(false);
+            bool continuePlaying = false;
+            if (PlayerControl.PlayerState == PlayerState.Playing)
+            {
+                continuePlaying = true;
+                PlayerControl.StopMedia();
+            }
+            if (ChangeSettings(dm))
+            {
+                m_RestoreFrequency = oldFreq;
+            }
+            if (continuePlaying)
+            {
+                PlayerControl.PlayMedia(false);
+            }
+            if (wasFullScreen)
+            {
+                PlayerControl.GoFullScreen();
+            }
         }
 
-        private static void ChangeSettings(DEVMODE dm)
+        private static bool ChangeSettings(DEVMODE dm)
         {
-            if (NativeMethods.ChangeDisplaySettingsEx(dm.dmDeviceName, ref dm, IntPtr.Zero, 0, IntPtr.Zero) !=
-                NativeMethods.DISP_CHANGE_SUCCESSFUL)
+            if (NativeMethods.ChangeDisplaySettingsEx(dm.dmDeviceName, ref dm, IntPtr.Zero,
+                    NativeMethods.CDS_UPDATEREGISTRY, IntPtr.Zero) == NativeMethods.DISP_CHANGE_SUCCESSFUL)
             {
-                Trace.Write("Failed to change display refresh rate");
+                NativeMethods.PostMessage(NativeMethods.HWND_BROADCAST, NativeMethods.WM_DISPLAYCHANGE,
+                    new IntPtr(NativeMethods.SPI_SETNONCLIENTMETRICS), IntPtr.Zero);
+                return true;
             }
+            Trace.Write("Failed to change display refresh rate");
+            return false;
         }
 
         private static int GetSettings(ref DEVMODE dm, string deviceName)
@@ -138,8 +252,13 @@ namespace Mpdn.PlayerExtensions.Example
         public DisplayChangerSettings()
         {
             Activate = false;
+            Restore = false;
+            Restricted = false;
         }
 
         public bool Activate { get; set; }
+        public bool Restore { get; set; }
+        public bool Restricted { get; set; }
+        public string VideoTypes { get; set; }
     }
 }
