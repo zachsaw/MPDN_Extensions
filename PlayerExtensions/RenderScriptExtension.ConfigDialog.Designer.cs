6554e98c1dce68dd083bf99ae0205c20da4a06b1
Shiandow <Shiandow@gmail.com>
2015-01-22 20:53:08 +0100

    Extend renderscript system with presets.
    Needs a renderscript that loads the active preset. Or some other way of
    controlling the active renderscript.
    
    By the way, do NOT try to make a preset for the renderscript that loads
    the active preset. Unless you like infinite recursion.

---
+++ PlayerExtensions/RenderScriptExtension.ConfigDialog.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Mpdn.PlayerExtensions.GitHub
+{
+    partial class RenderScriptExtensionDialog
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonConfigure = new System.Windows.Forms.Button();
+            this.menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuConfigure = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuRemove = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.presetGrid = new System.Windows.Forms.DataGridView();
+            this.scriptBox = new System.Windows.Forms.ComboBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.NameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RenderScriptColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DescriptionColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.presetGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonConfigure
+            // 
+            this.buttonConfigure.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonConfigure.Enabled = false;
+            this.buttonConfigure.Location = new System.Drawing.Point(18, 435);
+            this.buttonConfigure.Name = "buttonConfigure";
+            this.buttonConfigure.Size = new System.Drawing.Size(75, 23);
+            this.buttonConfigure.TabIndex = 5;
+            this.buttonConfigure.Text = "Configure...";
+            this.buttonConfigure.UseVisualStyleBackColor = true;
+            this.buttonConfigure.Click += new System.EventHandler(this.ButtonConfigureClick);
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuConfigure,
+            this.toolStripMenuItem1,
+            this.menuRemove});
+            this.menu.Name = "contextMenuStrip1";
+            this.menu.Size = new System.Drawing.Size(142, 54);
+            // 
+            // menuConfigure
+            // 
+            this.menuConfigure.Name = "menuConfigure";
+            this.menuConfigure.Size = new System.Drawing.Size(141, 22);
+            this.menuConfigure.Text = "Configure...";
+            this.menuConfigure.Click += new System.EventHandler(this.ButtonConfigureClick);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(138, 6);
+            // 
+            // menuRemove
+            // 
+            this.menuRemove.Name = "menuRemove";
+            this.menuRemove.ShortcutKeys = System.Windows.Forms.Keys.Delete;
+            this.menuRemove.Size = new System.Drawing.Size(141, 22);
+            this.menuRemove.Text = "Remove";
+            this.menuRemove.Click += new System.EventHandler(this.RemoveSelectedItem);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(681, 435);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 8;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOk.Location = new System.Drawing.Point(600, 435);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 7;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            // 
+            // presetGrid
+            // 
+            this.presetGrid.AllowUserToAddRows = false;
+            this.presetGrid.AllowUserToDeleteRows = false;
+            this.presetGrid.AllowUserToResizeRows = false;
+            this.presetGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.presetGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.presetGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NameColumn,
+            this.RenderScriptColumn,
+            this.DescriptionColumn});
+            this.presetGrid.ContextMenuStrip = this.menu;
+            this.presetGrid.Location = new System.Drawing.Point(18, 18);
+            this.presetGrid.Name = "presetGrid";
+            this.presetGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.presetGrid.Size = new System.Drawing.Size(738, 411);
+            this.presetGrid.TabIndex = 9;
+            this.presetGrid.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.CellValueChanged);
+            this.presetGrid.SelectionChanged += new System.EventHandler(this.SelectedIndexChanged);
+            this.presetGrid.DoubleClick += new System.EventHandler(this.ButtonConfigureClick);
+            // 
+            // scriptBox
+            // 
+            this.scriptBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scriptBox.FormattingEnabled = true;
+            this.scriptBox.Location = new System.Drawing.Point(180, 437);
+            this.scriptBox.Name = "scriptBox";
+            this.scriptBox.Size = new System.Drawing.Size(333, 21);
+            this.scriptBox.TabIndex = 10;
+            // 
+            // addButton
+            // 
+            this.addButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.addButton.Location = new System.Drawing.Point(99, 435);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 11;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.removeButton.Location = new System.Drawing.Point(519, 435);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.TabIndex = 12;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.RemoveSelectedItem);
+            // 
+            // NameColumn
+            // 
+            this.NameColumn.HeaderText = "Name";
+            this.NameColumn.Name = "NameColumn";
+            // 
+            // RenderScriptColumn
+            // 
+            this.RenderScriptColumn.FillWeight = 150F;
+            this.RenderScriptColumn.HeaderText = "Renderscript";
+            this.RenderScriptColumn.Name = "RenderScriptColumn";
+            this.RenderScriptColumn.ReadOnly = true;
+            this.RenderScriptColumn.Width = 150;
+            // 
+            // DescriptionColumn
+            // 
+            this.DescriptionColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.DescriptionColumn.HeaderText = "Description";
+            this.DescriptionColumn.Name = "DescriptionColumn";
+            this.DescriptionColumn.ReadOnly = true;
+            // 
+            // RenderScriptDialog
+            // 
+            this.AcceptButton = this.buttonOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(774, 476);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.scriptBox);
+            this.Controls.Add(this.presetGrid);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.buttonConfigure);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(500, 460);
+            this.Name = "RenderScriptDialog";
+            this.Padding = new System.Windows.Forms.Padding(15);
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Presets";
+            this.menu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.presetGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConfigure;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ContextMenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem menuConfigure;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem menuRemove;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.DataGridView presetGrid;
+        private System.Windows.Forms.ComboBox scriptBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RenderScriptColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DescriptionColumn;
+    }
+}  
