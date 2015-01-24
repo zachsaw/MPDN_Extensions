d001101d227e91db1523105a831e033561852d35
Zach Saw <zach.saw@gmail.com>
2015-01-24 23:29:20 +1100

    Make scripts compatible with v2.19.0

---
+++ RenderScripts/Mpdn.Presets.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Mpdn.RenderScript.Config;
+using Mpdn.PlayerExtensions.GitHub;
+using YAXLib;
+
+namespace Mpdn.RenderScript
+{
+    namespace Mpdn.Presets
+    {
+        public abstract class PresetRenderScript : IRenderScriptUi
+        {
+            protected abstract RenderScriptPreset Preset { get; }
+
+            protected virtual IRenderScriptUi Script { get { return Preset.Script ?? RenderScript.Empty; } }
+
+            public virtual IRenderScript CreateRenderScript()
+            {
+                return Script.CreateRenderScript();
+            }
+
+            public virtual void Destroy()
+            {
+                Script.Destroy();
+            }
+
+            public virtual void Initialize() { }
+
+            public virtual bool ShowConfigDialog(IWin32Window owner)
+            {
+                return Script.ShowConfigDialog(owner);
+            }
+
+            public virtual ScriptDescriptor Descriptor
+            {
+                get 
+                {
+                    var descriptor = Script.Descriptor;
+                    descriptor.Guid = Preset.Guid;
+                    return descriptor;
+                }
+            }
+        }
+
+        public class ActivePresetRenderScript : PresetRenderScript
+        {
+            private Guid m_Guid = new Guid("B1F3B882-3E8F-4A8C-B225-30C9ABD67DB1");
+
+            protected override RenderScriptPreset Preset 
+            {
+                get { return PresetExtension.ActivePreset ?? new RenderScriptPreset() { Script = RenderScript.Empty }; } 
+            }
+
+            public override void Initialize()
+            {
+                base.Initialize();
+
+                PresetExtension.ScriptGuid = m_Guid;
+            }
+
+            public override ScriptDescriptor Descriptor
+            {
+                get
+                {
+                    var descriptor = base.Descriptor;
+                    descriptor.Name = "Preset";
+                    descriptor.Guid = m_Guid;
+                    descriptor.Description = "Active Preset";
+                    return descriptor;
+                }
+            }
+        }
+    }
+}
--- src/Framework/Common.cs
+++ src/Framework/Common.cs
@@ -2,9 +2,55 @@ using System;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Windows.Forms;
 
 namespace Mpdn.RenderScript
 {
+    public static class RenderScript
+    {
+        public static IRenderScriptUi Empty = new NullRenderScriptUi();
+
+        public static bool IsEmpty(this IRenderScriptUi script)
+        {
+            return script is NullRenderScriptUi;
+        }
+
+        private class NullRenderScriptUi : IRenderScriptUi
+        {
+            public ScriptDescriptor Descriptor
+            {
+                get
+                {
+                    return new ScriptDescriptor
+                    {
+                        Guid = Guid.Empty,
+                        Name = "None",
+                        Description = "Do not use render script",
+                        HasConfigDialog = false
+                    };
+                }
+            }
+
+            public IRenderScript CreateRenderScript()
+            {
+                return null;
+            }
+
+            public void Initialize()
+            {
+            }
+
+            public void Destroy()
+            {
+            }
+
+            public bool ShowConfigDialog(IWin32Window owner)
+            {
+                return false;
+            }
+        }
+    }
+
     public static class Common
     {
         public static void Dispose(object obj)
