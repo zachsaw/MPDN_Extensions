8d0bc764c4ae65c09095bffc69dd257c0afaf4d6
Zach Saw <zach.saw@gmail.com>
2015-04-24 12:46:22 +1000

    Changed "Path" text to "Optimization".

--- Extensions/RenderScripts/Shiandow.Nnedi3.ConfigDialog.Designer.cs
+++ Extensions/RenderScripts/Shiandow.Nnedi3.ConfigDialog.Designer.cs
@@ -52,13 +52,14 @@ namespace Mpdn.RenderScript
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.comboBoxPath = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonCancel
             // 
             this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(174, 103);
+            this.buttonCancel.Location = new System.Drawing.Point(199, 132);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 23);
             this.buttonCancel.TabIndex = 1005;
@@ -69,7 +70,7 @@ namespace Mpdn.RenderScript
             // 
             this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.buttonOk.Location = new System.Drawing.Point(93, 103);
+            this.buttonOk.Location = new System.Drawing.Point(118, 132);
             this.buttonOk.Name = "buttonOk";
             this.buttonOk.Size = new System.Drawing.Size(75, 23);
             this.buttonOk.TabIndex = 1004;
@@ -86,7 +87,7 @@ namespace Mpdn.RenderScript
             "64",
             "128",
             "256"});
-            this.comboBoxNeurons.Location = new System.Drawing.Point(80, 17);
+            this.comboBoxNeurons.Location = new System.Drawing.Point(101, 17);
             this.comboBoxNeurons.Name = "comboBoxNeurons";
             this.comboBoxNeurons.Size = new System.Drawing.Size(67, 21);
             this.comboBoxNeurons.TabIndex = 0;
@@ -105,9 +106,9 @@ namespace Mpdn.RenderScript
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(24, 55);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.Size = new System.Drawing.Size(71, 13);
             this.label2.TabIndex = 1009;
-            this.label2.Text = "Path:";
+            this.label2.Text = "Optimization*:";
             // 
             // comboBoxPath
             // 
@@ -117,10 +118,20 @@ namespace Mpdn.RenderScript
             "Prefer Scalar",
             "Prefer Vector",
             "Avoid Branches"});
-            this.comboBoxPath.Location = new System.Drawing.Point(80, 52);
+            this.comboBoxPath.Location = new System.Drawing.Point(101, 52);
             this.comboBoxPath.Name = "comboBoxPath";
             this.comboBoxPath.Size = new System.Drawing.Size(152, 21);
             this.comboBoxPath.TabIndex = 1008;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 26);
+            this.label3.TabIndex = 1010;
+            this.label3.Text = "* Try all optimization options and select \r\n   the fastest for the chosen neurons" +
+    "";
             // 
             // NNedi3ConfigDialog
             // 
@@ -128,7 +139,8 @@ namespace Mpdn.RenderScript
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(261, 138);
+            this.ClientSize = new System.Drawing.Size(286, 167);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBoxPath);
             this.Controls.Add(this.label1);
@@ -155,6 +167,7 @@ namespace Mpdn.RenderScript
             private System.Windows.Forms.Label label1;
             private System.Windows.Forms.Label label2;
             private System.Windows.Forms.ComboBox comboBoxPath;
+            private System.Windows.Forms.Label label3;
 
         }
     }
