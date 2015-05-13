cb8505f3050e081ce2decccc5332d77739150522
Zach Saw <zach.saw@gmail.com>
2015-05-13 15:52:07 +1000

    Fixed script load errors caused by previous check-in by adding a reference to Microsoft.CSharp which ScriptedRenderChain.cs requires.

--- Extensions/RenderScripts/Mpdn.ScriptedRenderChain.cs
+++ Extensions/RenderScripts/Mpdn.ScriptedRenderChain.cs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library.
 // 
+//css_reference Microsoft.CSharp;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
