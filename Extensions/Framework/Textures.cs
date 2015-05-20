7050d84e8c114c1ac924471021e24fed37c6b5f3
Shiandow <Shiandow@gmail.com>
2015-05-20 15:19:29 +0200

    Garbage collect SourceFilters with TexturePool

--- Extensions/Framework/Textures.cs
+++ Extensions/Framework/Textures.cs
@@ -115,15 +115,15 @@ namespace Mpdn.RenderScript
 
     public static class TexturePool
     {
-        private static List<IBaseTexture> m_SourceTextures = new List<IBaseTexture>();
-        private static List<ITargetTexture> m_OldTextures = new List<ITargetTexture>();
-        private static List<ITargetTexture> m_SavedTextures = new List<ITargetTexture>();
-        private static List<ITargetTexture> m_TempTextures = new List<ITargetTexture>();
-        private bool m_Disposed;
+        private static List<IBaseTexture> OldSourceTextures = new List<IBaseTexture>();
+        private static List<IBaseTexture> SourceTextures = new List<IBaseTexture>();
+        private static List<ITargetTexture> OldTextures = new List<ITargetTexture>();
+        private static List<ITargetTexture> SavedTextures = new List<ITargetTexture>();
+        private static List<ITargetTexture> TempTextures = new List<ITargetTexture>();
 
         public static ITargetTexture GetTexture(TextureSize textureSize, TextureFormat? textureFormat = null)
         {
-            foreach (var list in new[] { m_SavedTextures, m_OldTextures })
+            foreach (var list in new[] { SavedTextures, OldTextures })
             {
                 var index = list.FindIndex(x => (x.GetSize() == textureSize) && (x.Format == textureFormat));
                 if (index < 0) continue;
@@ -139,43 +139,59 @@ namespace Mpdn.RenderScript
         public static ISourceTexture GetSourceTexture(TextureSize textureSize, TextureFormat? textureFormat = null)
         {
             var texture = Renderer.CreateTexture(textureSize.Width, textureSize.Height, textureFormat ?? Renderer.RenderQuality.GetTextureFormat());
-            m_SourceTextures.Add(texture);
+            SourceTextures.Add(texture);
             return texture;
         }
 
         public static ISourceTexture3D GetSourceTexture3D(TextureSize textureSize, TextureFormat? textureFormat = null)
         {
             var texture = Renderer.CreateTexture3D(textureSize.Width, textureSize.Height, textureSize.Depth, textureFormat ?? Renderer.RenderQuality.GetTextureFormat());
-            m_SourceTextures.Add(texture);
+            SourceTextures.Add(texture);
             return texture;
         }
 
         public static void PutTempTexture(ITargetTexture texture)
         {
-            m_TempTextures.Add(texture);
-            m_SavedTextures.Add(texture);
+            TempTextures.Add(texture);
+            SavedTextures.Add(texture);
         }
 
         public static void PutTexture(ITargetTexture texture)
         {
-            m_SavedTextures.Add(texture);
+            SavedTextures.Add(texture);
+        }
+
+        public static void MarkUsed(IBaseTexture texture)
+        {
+            if (OldSourceTextures.Remove(texture))
+                SourceTextures.Add(texture);
         }
 
         public static void FlushTextures()
         {
-            foreach (var texture in m_OldTextures)
+            foreach (var texture in OldTextures)
             {
                 DisposeHelper.Dispose(texture);
             }
 
-            foreach (var texture in m_TempTextures)
+            foreach (var texture in OldSourceTextures)
             {
-                m_SavedTextures.Remove(texture);
+                DisposeHelper.Dispose(texture);
             }
 
-            m_OldTextures = m_SavedTextures;
-            m_TempTextures = new List<ITargetTexture>();
-            m_SavedTextures = new List<ITargetTexture>();
+            foreach (var texture in TempTextures)
+            {
+                SavedTextures.Remove(texture);
+            }
+
+            OldTextures.Clear();
+            OldSourceTextures.Clear();
+            OldTextures.AddRange(SavedTextures);
+            OldSourceTextures.AddRange(SourceTextures);
+
+            TempTextures.Clear();
+            SavedTextures.Clear();
+            SourceTextures.Clear();
         }
     }
 }
