d2d5352f6f8101778e17a12d5e630391fd49fe81
Zach Saw <zach.saw@gmail.com>
2015-06-07 22:26:20 +1000

    Make.bat now makes a zip file for release.

--- Make.bat
+++ Make.bat
@@ -10,6 +10,12 @@ mkdir Release 1>nul 2>nul
 
 set buildPlatform=Release
 
+set zipper="%ProgramFiles%\7-zip\7z.exe"
+if not exist %zipper% (
+  echo Error: 7-zip ^^^(native version^^^) is not installed
+  goto Quit
+)
+
 for /D %%D in (%SYSTEMROOT%\Microsoft.NET\Framework\v4*) do set msbuild.exe=%%D\MSBuild.exe
 if not defined msbuild.exe echo error: can't find MSBuild.exe & goto Quit
 if not exist "%msbuild.exe%" echo error: %msbuild.exe%: not found & goto Quit
@@ -21,12 +27,20 @@ Echo.
 
 if not "%ERRORLEVEL%"=="0" echo error: build failed & goto Quit
 
-xcopy /y bin\Release\Mpdn.Extensions.dll Release\  1>nul 2>nul
-xcopy /y bin\Release\ClearScript.dll Release\  1>nul 2>nul
-xcopy /y bin\Release\ICSharpCode.AvalonEdit.dll Release\  1>nul 2>nul
+xcopy /y bin\Release\Mpdn.Extensions.dll Release\Extensions\  1>nul 2>nul
+
+echo .cs\ > excludedfiles.txt
+xcopy /y /e /exclude:excludedfiles.txt "Extensions\RenderScripts\*.*" "Release\Extensions\RenderScripts\" 1>nul 2>nul
+del excludedfiles.txt
+
+echo Zipping up...
+Echo.
+cd "Release"
+%zipper% a -r -tzip -mx9 "%~dp0Release\Mpdn.Extensions.zip" * > NUL
+rmdir /q /s Extensions 1>nul 2>nul
 
 Echo Completed successfully.
-start Release\
+start .
 
 :Quit
 Echo.
