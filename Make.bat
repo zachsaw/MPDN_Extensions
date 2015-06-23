013d9057c988a7d2edbe67bfb2301557b346522d
Antoine Aflalo <antoine@aaflalo.me>
2015-06-23 15:40:01 +0200

    [MOD] Automate the script It now takes the latest tag and the count of commits

--- Make.bat
+++ Make.bat
@@ -1,14 +1,18 @@
 @echo off
-if "%1"=="" (
-  echo Usage: Make.bat [version x.y.z.rev] (e.g. Make.bat 1.2.3.456^)
-  echo        where 'rev' is the commit count on GitHub as of this revision
-  goto Quit
-)
+REM if "%1"=="" (
+REM   echo Usage: Make.bat [version x.y.z.rev] (e.g. Make.bat 1.2.3.456^)
+REM   echo        where 'rev' is the commit count on GitHub as of this revision
+  REM goto Quit
+REM )
 
 setlocal
 
 cd "%~dp0"
-set releaseVersion=%1
+git describe --abbrev=0 --tags > latestTag.txt
+for /f "delims=" %%i in ('git rev-list HEAD --count') do set commitCount=%%i
+set /p latestTag=<latestTag.txt
+delete latestTag.txt
+set releaseVersion=%latestTag%.%commitCount%
 
 if not exist "MPDN\Mpdn.Core.dll" (
     echo Error: Make sure you've copied MPDN into the MPDN folder first!
