e42c3c2c9ecdae53f6298b7ea8c53350b3ca2ce1
Antoine Aflalo <antoine@aaflalo.me>
2015-06-25 11:26:25 +0200

    [MOD] Retrieve the extension version from assembly Use it for the User-Agent

--- Extensions/PlayerExtensions/UpdateChecker.PlayerExtension.cs
+++ Extensions/PlayerExtensions/UpdateChecker.PlayerExtension.cs
@@ -129,9 +129,19 @@ namespace Mpdn.Extensions.PlayerExtensions
             m_WebClient.DownloadStringCompleted += DownloadStringCompleted;
         }
 
+        public static Version GetExtensionsVersion()
+        {
+            var asm = typeof(UpdateChecker).Assembly;
+            var ver = FileVersionInfo.GetVersionInfo(asm.Location);
+            return new Version(ver.ToString());
+        }
+
         private void SetHeaders()
         {
-            m_WebClient.Headers.Add("User-Agent", string.Format("MPDN/{0} (+http://mpdn.zachsaw.com/)", Application.ProductVersion));
+            var version = GetExtensionsVersion();
+
+            m_WebClient.Headers.Add("User-Agent", string.Format("Mozilla/5.0 (compatible; Windows NT {0}; MPDN/{1}; MPDN_Extensions/{2}; +http://mpdn.zachsaw.com/)",
+                    Environment.OSVersion.Version, Application.ProductVersion, version));
         }
 
         private void DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
