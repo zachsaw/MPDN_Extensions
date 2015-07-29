4a31e77b2be9b5845e2b033956af80e70025d01b
Garteal <Garteal@gmail.com>
2015-07-29 17:49:32 +0200

    Updated project file

--- Extensions/PlayerExtensions/PlaylistForm.cs
+++ Extensions/PlayerExtensions/PlaylistForm.cs
@@ -24,7 +24,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Runtime.Remoting.Contexts;
 using System.Security;
 using System.Text;
 using System.Text.RegularExpressions;
