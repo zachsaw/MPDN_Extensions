94ffe339edd22c5f5bbf7ac284d527c70f775397
zachsaw <Zach Saw>
2015-08-14 22:24:11 +1000

    [Lut3D] config dialog not scaling properly on Windows with higher DPI.

--- Extensions/RenderScripts/Mpdn.Lut3D.ConfigDialog.Designer.cs
+++ Extensions/RenderScripts/Mpdn.Lut3D.ConfigDialog.Designer.cs
@@ -63,7 +63,7 @@ namespace Mpdn.Extensions.RenderScripts
             this.ButtonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.ButtonOK.Cursor = System.Windows.Forms.Cursors.Default;
             this.ButtonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.ButtonOK.Location = new System.Drawing.Point(204, 93);
+            this.ButtonOK.Location = new System.Drawing.Point(270, 93);
             this.ButtonOK.Name = "ButtonOK";
             this.ButtonOK.Size = new System.Drawing.Size(75, 23);
             this.ButtonOK.TabIndex = 1000;
@@ -76,7 +76,7 @@ namespace Mpdn.Extensions.RenderScripts
             this.ButtonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.ButtonCancel.Cursor = System.Windows.Forms.Cursors.Default;
             this.ButtonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.ButtonCancel.Location = new System.Drawing.Point(285, 93);
+            this.ButtonCancel.Location = new System.Drawing.Point(351, 93);
             this.ButtonCancel.Name = "ButtonCancel";
             this.ButtonCancel.Size = new System.Drawing.Size(75, 23);
             this.ButtonCancel.TabIndex = 1001;
@@ -103,14 +103,14 @@ namespace Mpdn.Extensions.RenderScripts
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxFileName.Location = new System.Drawing.Point(44, 43);
             this.textBoxFileName.Name = "textBoxFileName";
-            this.textBoxFileName.Size = new System.Drawing.Size(275, 20);
+            this.textBoxFileName.Size = new System.Drawing.Size(341, 20);
             this.textBoxFileName.TabIndex = 1;
             this.textBoxFileName.Validating += new System.ComponentModel.CancelEventHandler(this.TextBoxFileNameValidating);
             // 
             // buttonOpen
             // 
             this.buttonOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonOpen.Location = new System.Drawing.Point(325, 41);
+            this.buttonOpen.Location = new System.Drawing.Point(391, 41);
             this.buttonOpen.Name = "buttonOpen";
             this.buttonOpen.Size = new System.Drawing.Size(28, 23);
             this.buttonOpen.TabIndex = 2;
@@ -139,17 +139,16 @@ namespace Mpdn.Extensions.RenderScripts
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.ButtonCancel;
-            this.ClientSize = new System.Drawing.Size(372, 128);
+            this.ClientSize = new System.Drawing.Size(438, 128);
             this.Controls.Add(this.checkBoxActivate);
             this.Controls.Add(this.buttonOpen);
             this.Controls.Add(this.textBoxFileName);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ButtonCancel);
             this.Controls.Add(this.ButtonOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(10000000, 167);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(300, 167);
             this.Name = "Lut3DConfigDialog";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
