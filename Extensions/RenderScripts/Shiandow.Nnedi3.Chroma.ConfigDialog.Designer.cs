6466dcbde5f89d07ff2fdb1803b2ad5ca984a993
Zach Saw <zach.saw@gmail.com>
2015-09-02 15:26:57 +1000

    [NNEDI3 Chroma Scaler] Add the actual chroma scaler files

---
+++ Extensions/RenderScripts/Shiandow.Nnedi3.Chroma.ConfigDialog.Designer.cs
@@ -0,0 +1,164 @@
+// This file is a part of MPDN Extensions.
+// https://github.com/zachsaw/MPDN_Extensions
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3.0 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library.
+// 
+namespace Mpdn.Extensions.RenderScripts
+{
+    namespace Shiandow.NNedi3.Chroma
+    {
+        partial class NNedi3ConfigDialog
+        {
+            /// <summary>
+            /// Required designer variable.
+            /// </summary>
+            private System.ComponentModel.IContainer components = null;
+
+            /// <summary>
+            /// Clean up any resources being used.
+            /// </summary>
+            /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+            protected override void Dispose(bool disposing)
+            {
+                if (disposing && (components != null))
+                {
+                    components.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+
+            #region Windows Form Designer generated code
+
+            /// <summary>
+            /// Required method for Designer support - do not modify
+            /// the contents of this method with the code editor.
+            /// </summary>
+            private void InitializeComponent()
+            {
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxPath = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBoxStructured = new System.Windows.Forms.CheckBox();
+            this.neuronsSelector = new RenderScripts.NeuronsSelector();
+            this.SuspendLayout();
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(173, 285);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 1005;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOk.Location = new System.Drawing.Point(92, 285);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 1004;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 1009;
+            this.label2.Text = "Optimization*:";
+            // 
+            // comboBoxPath
+            // 
+            this.comboBoxPath.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPath.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBoxPath.FormattingEnabled = true;
+            this.comboBoxPath.Location = new System.Drawing.Point(54, 197);
+            this.comboBoxPath.Name = "comboBoxPath";
+            this.comboBoxPath.Size = new System.Drawing.Size(152, 21);
+            this.comboBoxPath.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 26);
+            this.label3.TabIndex = 1010;
+            this.label3.Text = "* Try all optimization options and select \r\n   the fastest for the chosen neurons" +
+    "";
+            // 
+            // checkBoxStructured
+            // 
+            this.checkBoxStructured.AutoSize = true;
+            this.checkBoxStructured.Location = new System.Drawing.Point(54, 174);
+            this.checkBoxStructured.Name = "checkBoxStructured";
+            this.checkBoxStructured.Size = new System.Drawing.Size(177, 17);
+            this.checkBoxStructured.TabIndex = 3;
+            this.checkBoxStructured.Text = "Alternate weight access method";
+            this.checkBoxStructured.UseVisualStyleBackColor = true;
+            this.checkBoxStructured.CheckedChanged += new System.EventHandler(this.StructuredCheckedChanged);
+            // 
+            // neuronsSelector
+            // 
+            this.neuronsSelector.Location = new System.Drawing.Point(27, 19);
+            this.neuronsSelector.Name = "neuronsSelector";
+            this.neuronsSelector.Size = new System.Drawing.Size(175, 123);
+            this.neuronsSelector.TabIndex = 0;
+            // 
+            // NNedi3ConfigDialog
+            // 
+            this.AcceptButton = this.buttonOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.ClientSize = new System.Drawing.Size(260, 320);
+            this.Controls.Add(this.neuronsSelector);
+            this.Controls.Add(this.checkBoxStructured);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxPath);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOk);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "NNedi3ConfigDialog";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "NNEDI3 Configuration";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            }
+
+            #endregion
+
+            private System.Windows.Forms.Button buttonCancel;
+            private System.Windows.Forms.Button buttonOk;
+            private System.Windows.Forms.Label label2;
+            private System.Windows.Forms.ComboBox comboBoxPath;
+            private System.Windows.Forms.Label label3;
+            private System.Windows.Forms.CheckBox checkBoxStructured;
+            private RenderScripts.NeuronsSelector neuronsSelector;
+        }
+    }
+}
---
+++ Extensions/RenderScripts/Shiandow.Nnedi3.Chroma.Scaler.cs
@@ -0,0 +1,143 @@
+﻿// This file is a part of MPDN Extensions.
+// https://github.com/zachsaw/MPDN_Extensions
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3.0 of the License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library.
+
+using System;
+using Mpdn.Extensions.Framework;
+using Mpdn.Extensions.Framework.RenderChain;
+using Mpdn.Extensions.RenderScripts.Shiandow.NNedi3.Filters;
+using Mpdn.RenderScript;
+using SharpDX;
+
+namespace Mpdn.Extensions.RenderScripts
+{
+    namespace Shiandow.NNedi3.Chroma
+    {
+        public class NNedi3 : RenderChain, IChromaScaler
+        {
+            #region Settings
+
+            public NNedi3()
+            {
+                Neurons1 = NNedi3Neurons.Neurons16;
+                Neurons2 = NNedi3Neurons.Neurons16;
+                CodePath = NNedi3Path.ScalarMad;
+                Structured = false;
+            }
+
+            public NNedi3Neurons Neurons1 { get; set; }
+            public NNedi3Neurons Neurons2 { get; set; }
+            public NNedi3Path CodePath { get; set; }
+            public bool Structured { get; set; }
+
+            #endregion
+
+            private static readonly int[] s_NeuronCount = {16, 32, 64, 128, 256};
+            private static readonly string[] s_CodePath = {"A", "B", "C", "D", "E"};
+
+            private Nnedi3Filter m_UFilter1;
+            private Nnedi3Filter m_UFilter2;
+            private Nnedi3Filter m_VFilter1;
+            private Nnedi3Filter m_VFilter2;
+
+            public override void Reset()
+            {
+                base.Reset();
+                Cleanup();
+            }
+
+            private void Cleanup()
+            {
+                DisposeHelper.Dispose(ref m_UFilter1);
+                DisposeHelper.Dispose(ref m_UFilter2);
+                DisposeHelper.Dispose(ref m_VFilter1);
+                DisposeHelper.Dispose(ref m_VFilter2);
+            }
+
+            public override IFilter CreateFilter(IFilter input)
+            {
+                var chromaFilter = input as ChromaFilter;
+                if (chromaFilter != null)
+                    chromaFilter.ChromaScaler = this;
+                return input;
+            }
+
+            private string GetShaderFileName(NNedi3Neurons neurons, bool u)
+            {
+                return string.Format("NNEDI3_{3}_{0}_{1}{2}.cso", s_NeuronCount[(int) neurons], s_CodePath[(int) CodePath],
+                    Structured ? "_S" : string.Empty, u ? "u" : "v");
+            }
+
+            public IFilter CreateChromaFilter(IFilter lumaInput, IFilter chromaInput, Vector2 chromaOffset)
+            {
+                Cleanup();
+
+                if (!Renderer.IsDx11Avail)
+                {
+                    Renderer.FallbackOccurred = true; // Warn user via player stats OSD
+                    return new ChromaFilter(lumaInput, chromaInput, null, chromaOffset); // DX11 is not available; fallback
+                }
+
+                Func<TextureSize, TextureSize> transform = s => new TextureSize(2 * s.Height, s.Width);
+
+                var shaderUPass1 = LoadShader11(GetShaderFileName(Neurons1, true));
+                var shaderUPass2 = LoadShader11(GetShaderFileName(Neurons2, true));
+                var shaderVPass1 = LoadShader11(GetShaderFileName(Neurons1, false));
+                var shaderVPass2 = LoadShader11(GetShaderFileName(Neurons2, false));
+                var interleaveU = CompileShader("Interleave.hlsl", macroDefinitions: "CHROMA_U=1").Configure(transform: transform);
+                var interleaveV = CompileShader("Interleave.hlsl", macroDefinitions: "CHROMA_V=1").Configure(transform: transform);
+
+                m_UFilter1 = NNedi3Helpers.CreateFilter(shaderUPass1, chromaInput, Neurons1, Structured);
+                var resultU = new ShaderFilter(interleaveU, chromaInput, m_UFilter1);
+                m_UFilter2 = NNedi3Helpers.CreateFilter(shaderUPass2, resultU, Neurons2, Structured);
+                var u = new ShaderFilter(interleaveU, resultU, m_UFilter2);
+
+                m_VFilter1 = NNedi3Helpers.CreateFilter(shaderVPass1, chromaInput, Neurons1, Structured);
+                var resultV = new ShaderFilter(interleaveV, chromaInput, m_VFilter1);
+                m_VFilter2 = NNedi3Helpers.CreateFilter(shaderVPass2, resultV, Neurons2, Structured);
+                var v = new ShaderFilter(interleaveV, resultV, m_VFilter2);
+
+                return new MergeFilter(lumaInput, u, v);
+            }
+        }
+
+        public class NNedi3ChromaScaler : RenderChainUi<NNedi3, NNedi3ConfigDialog>
+        {
+            protected override string ConfigFileName
+            {
+                get { return "Shiandow.NNedi3Chroma"; }
+            }
+
+            public override string Category
+            {
+                get { return "Chroma Scaling"; }
+            }
+
+            public override ExtensionUiDescriptor Descriptor
+            {
+                get
+                {
+                    return new ExtensionUiDescriptor
+                    {
+                        Guid = new Guid("994C176F-AB9F-47E0-81FE-DC20609A40C2"),
+                        Name = "NNEDI3 Chroma Doubler",
+                        Description = "NNEDI3 chroma doubler",
+                        Copyright = "Adapted by Shiandow and Zachs for MPDN"
+                    };
+                }
+            }
+        }
+    }
+}
