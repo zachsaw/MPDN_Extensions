18fbf025436d03c2ae8c4af607b3952227f7c28d
Zach Saw <zach.saw@gmail.com>
2015-09-25 15:47:48 +1000

    [Reclock] Increase aggressiveness to allow convergence to occur sooner

--- Extensions/AudioScripts/Mpdn.Reclock.cs
+++ Extensions/AudioScripts/Mpdn.Reclock.cs
@@ -29,8 +29,8 @@ namespace Mpdn.Extensions.AudioScripts
             private const double MAX_PERCENT_ADJUST = 3; // automatically reclock if the difference is less than 3%
             private const double SANEAR_OVERSHOOT = 15;
             private const double DIRECTSOUND_OVERSHOOT = 10;
-            private const double MAX_SWING = 0.00025;
-            private const int RATIO_ADJUST_INTERVAL = 128*1024;
+            private const double MAX_SWING = 0.0005;
+            private const int RATIO_ADJUST_INTERVAL = 64*1024;
 
             private static readonly Guid s_SanearSoundClsId = new Guid("DF557071-C9FD-433A-9627-81E0D3640ED9");
             private static readonly Guid s_DirectSoundClsId = new Guid("79376820-07D0-11CF-A24D-0020AFD79767");
