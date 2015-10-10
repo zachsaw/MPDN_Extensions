876544ed4d5b1e50a86975cfd12677023bacbc70
Shiandow <Shiandow@gmail.com>
2015-10-10 21:48:29 +0200

    [SuperRes] Some tweaks
    Still experimental, but will push them anyway to move things out of the
    way of the framework changes.

--- Extensions/RenderScripts/SuperRes/SuperResEx.hlsl
+++ Extensions/RenderScripts/SuperRes/SuperResEx.hlsl
@@ -22,9 +22,6 @@
     #define softness (args0[1])
 #endif
 
-#define Pass args0[2]
-#define Passes args0[3]
-
 // -- Misc --
 sampler s0    : register(s0);
 sampler sDiff : register(s1);
@@ -46,6 +43,13 @@ float4 args0  : register(c3);
 #define dxdy (p1.xy)
 #define ddxddy (originalSize.zw)
 
+// -- Window Size --
+#define taps 4
+#define even (taps - 2 * (taps / 2) == 0)
+#define minX (1-ceil(taps/2.0))
+#define maxX (floor(taps/2.0))
+
+// -- Convenience --
 #define sqr(x) dot(x,x)
 
 // -- Colour space Processing --
@@ -53,7 +57,7 @@ float4 args0  : register(c3);
 
 // -- Input processing --
 //Current high res value
-#define Get(x,y)    (tex2D(s0,tex + dxdy*int2(x,y)).xyz)
+#define Get(x,y)    (tex2D(s0,tex + sqrt(ddxddy/dxdy)*dxdy*int2(x,y)).xyz)
 #define GetY(x,y)    (tex2D(sDiff,ddxddy*(pos+int2(x,y)+0.5)).a)
 //Downsampled result
 #define Diff(x,y)     (tex2D(sDiff,ddxddy*(pos+int2(x,y)+0.5)))
@@ -61,39 +65,56 @@ float4 args0  : register(c3);
 // -- Main Code --
 float4 main(float2 tex : TEXCOORD0) : COLOR{
     float4 c0 = tex2D(s0, tex);
+    float4 Lin = c0;
     c0.xyz = Gamma(c0.xyz);
 
     // Calculate position
     float2 pos = tex * originalSize.xy - 0.5;
-    float2 offset = pos - round(pos);
+    float2 offset = pos - (even ? floor(pos) : round(pos));
     pos -= offset;
 
     // Calculate faithfulness force
     float weightSum = 0;
     float4 diff = 0;
-    float inf = 1;
-    float sup = 0;
-
-    [unroll] for (int X = -1; X <= 1; X++)
-    [unroll] for (int Y = -1; Y <= 1; Y++)
+    float3 soft = 0;
+   
+    [unroll] for (int X = minX; X <= maxX; X++)
+    [unroll] for (int Y = minX; Y <= maxX; Y++)
     {
         float dI2 = sqr(acuity*(Luma(c0) - GetY(X,Y)));
-        float dXY2 = sqr(float2(X,Y) - offset);
-        float weight = (exp(-dXY2/(2*radius*radius)) - exp(-sqr(float2(1.5,1.5))/(2*radius*radius)))*pow(1 + dI2/power, - power);
+        float dXY2 = sqr((float2(X,Y) - offset)/radius);
+        float weight = exp(-0.5*dXY2) * pow(1 + dI2/power, - power);
 
         diff += weight*Diff(X,Y);
         weightSum += weight;
-        inf = min(GetY(X,Y),inf.x);
-        sup = max(GetY(X,Y),sup.x);
     }
     diff /= weightSum;
     c0.xyz -= strength * diff;
 
-    // Convert back to linear light;
 #ifndef FinalPass
-    // if (frac(p0.z/2.0) < 0.5) 
-    c0.xyz += softness*(clamp(Luma(c0.xyz), inf, sup) - Luma(c0.xyz));
+    weightSum=0;
+    #define softAcuity 6.0
+    #define softPower 1.5
+
+    [unroll] for (int X = -1; X <= 1; X++)
+    [unroll] for (int Y = -1; Y <= 1; Y++)
+    if (X != 0 || Y != 0)
+    {
+        float3 dI = Get(X,Y) - Lin;
+        float dI2 = sqr(softAcuity*dI);
+        float dXY2 = sqr(float2(X,Y)/radius);
+        float weight = pow(rsqrt(dXY2 + dI2),3); // Fundamental solution to the 5d Laplace equation
+        // float weight = exp(-0.5*dXY2) * pow(1 + dI2/power, - power);
+
+        soft += weight * dI;
+        weightSum += weight;
+    }
+    soft /= weightSum;
+
+    // Convert back to linear light;
     c0.xyz = GammaInv(c0.xyz);
+
+    c0.xyz += softness * soft;
 #endif
 
     return c0;
