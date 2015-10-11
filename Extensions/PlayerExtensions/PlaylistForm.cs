078e8036ebd8c1779563537306f079ba0c0513c2
Garteal <Garteal@gmail.com>
2015-10-11 23:49:14 +0200

    Fixup PlayNextFileInDirectory preloading

--- Extensions/PlayerExtensions/PlaylistForm.cs
+++ Extensions/PlayerExtensions/PlaylistForm.cs
@@ -840,9 +840,7 @@ namespace Mpdn.Extensions.PlayerExtensions.Playlist
 
             if (nextFile == null) return;
 
-            Media.Open(nextFile);
-
-            if (Playlist.Count == 1) ActiveFile(nextFile);
+            if (Playlist.Count == 1) ActiveFile(nextFile, true);
             else OpenFiles(new[] {nextFile});
         }
 
@@ -923,7 +921,7 @@ namespace Mpdn.Extensions.PlayerExtensions.Playlist
             OpenMedia(true);
         }
 
-        public void ActiveFile(string fileName)
+        public void ActiveFile(string fileName, bool play = false)
         {
             ResetActive();
             var item = new PlaylistItem(fileName, true);
@@ -931,6 +929,8 @@ namespace Mpdn.Extensions.PlayerExtensions.Playlist
             Playlist.Add(item);
             CurrentItem = item;
             PopulatePlaylist();
+
+            if (play) OpenMedia();
 
             if (!Duration.Visible) return;
             Task.Factory.StartNew(GetCurrentMediaDuration);
