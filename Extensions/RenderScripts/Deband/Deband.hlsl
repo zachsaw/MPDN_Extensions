d82a760ac9009a7b8854f9a46943b9c305f10a4a
Shiandow <Shiandow@gmail.com>
2016-10-09 22:29:53 +0200

    Deband: New approach.
    Seems to be able to preserve nearly all detail, while removing "all"
    banding (apart from source artefacts).
    
    Requires more testing, and some more controls.

--- Extensions/RenderScripts/Deband/Deband.hlsl
+++ Extensions/RenderScripts/Deband/Deband.hlsl
@@ -16,17 +16,19 @@
 // 
 sampler s0 : register(s0);
 sampler s1 : register(s1);
-float4 p0 : register(c0);
-float4 p1 : register(c1);
 float4 args0 : register(c2);
 float4 size0 : register(c3);
 float4 size1 : register(c4);
+float4 sizeOutput : register(c5);
 
-#define ppx (size1[2])
-#define ppy (size1[3])
+#define dxdy size0.zw
+#define ddxddy size1.zw
 
 #define acuity args0[0]
 #define power  args0[1]
+
+#define pi acos(-1)
+#define sqr(x) ((x)*(x))
 
 /* Noise between -0.5 and 0.5 */
 float noise(float2 c0){
@@ -51,54 +53,64 @@ float tempNoise(float2 tex, float t)
 }
 
 // Input Processing
-#define Get(x,y)  	(tex2D(s1,float2(ppx,ppy)*(pos + 0.5 + float2(x,y))))
+#define GetXY(xy) 	(tex2D(s1,ddxddy*(pos + 0.5 + (xy))))
+#define Get(x,y)  	(GetXY(float2(x,y)))
+
+#define GetHR(x,y)  	(tex2D(s0, tex + dxdy*float2(x, y)))
 
 float4 main(float2 tex : TEXCOORD0) : COLOR {
 	float4 c0 = tex2D(s0, tex);
 
 	float2 pos = tex * size1.xy - 0.5;
-	float2 offset = frac(pos);
+	float2 offset = pos - clamp(floor(pos), 0, size1-2);
 	pos -= offset;
 
 	// Load input
-	float4x4 X = {Get(0,0) - c0, Get(1,0) - c0, Get(0,1) - c0, Get(1,1) - c0};
+	float4x4 X = {Get(0,0), Get(1,0), Get(0,1), Get(1,1)};
 	
 	// Use linear regression to interpolate
-	float3x4 LinFit = {{-2, 2, -2, 2}, {-2, -2, 2, 2}, {1, 1, 1, 1}};
-	float4 w = 0.25*mul(float1x3(offset-0.5,1), LinFit);
-	float4 avg = c0 + mul(w,X);
+	float3x4 LinFit = {{-0.5, 0.5, -0.5, 0.5}, {-0.5, -0.5, 0.5, 0.5}, {0.25, 0.25, 0.25, 0.25}};
+	float4 w = mul(float1x3(offset-0.5,1), LinFit);
+	float4 avg = mul(w,X);
 
 	// Statistical analysis
-	X *= acuity;
-	#define sqr(x) ((x)*(x))
-	float3 SSres = sqr(mul(float4(0.5,-0.5,-0.5,0.5),X).xyz); // Residual sum of squares
-	float3 SStot = (sqr(X[0].xyz) + sqr(X[1].xyz) + sqr(X[2].xyz) + sqr(X[3].xyz)) - sqr(mul(float4(0.5,0.5,0.5,0.5),X).xyz); // Total sum of squares
+	float4x4 Y = (X - float4x4(c0, c0, c0, c0))*acuity;
+	float3 SSres = sqr(mul(float4(0.5,-0.5,-0.5,0.5),Y).xyz); // Residual sum of squares
+	float3 SStot = (sqr(Y[0].xyz) + sqr(Y[1].xyz) + sqr(Y[2].xyz) + sqr(Y[3].xyz)) - sqr(mul(float4(0.5,0.5,0.5,0.5),Y).xyz); // Total sum of squares
 	float3 R = 1 - (SSres/SStot); // Coefficient of determination
 
-	// Calculate variance of current value
-	float3 p = saturate(abs(c0 - avg)*acuity);
-	float3 varX = p*(1-p);
-
-	// Include current value (minus noise) as sample
-	float n = 5; // samples
-	float k = 3; // degrees of freedom
-	SSres = (SSres + max(0, sqr((c0 - avg)*acuity) - varX))/(n-k);
-
 	// Calculate variance of debanded value
-	float3 varY = SSres;
+	float3 varY = SSres * (1.0/4.0 + dot(offset-0.5, offset-0.5));
 
-	// Merge with high res values
-	float3 str = varX/(varX + varY);
-	c0.rgb = lerp(c0, avg, str);
+	float4x2 grad = transpose(mul(float2x4(LinFit[0], LinFit[1]), Y));
+	float4 grad2 = float4(dot(grad[0], grad[0]), dot(grad[1], grad[1]), dot(grad[2], grad[2]), dot(grad[3], grad[3]));
+
+	float3 m = (avg-c0)*acuity;
+	float3 diff = 0.5 * sign(m) * max(0, abs(m)+1 - sqrt(sqr(abs(m)-1) + 4*varY)); // Maximum likelihood estimate
+
+	// varY += 1e-10; // Add noise to avoid numerical issues
+	// float3 p = 2 * saturate(1 - abs(diff)) * rsqrt(2*pi*varY) * exp(-0.5*sqr(diff - m)/varY); // Posterior Probability
+
+#define PRESERVE_DETAIL
+#ifdef PRESERVE_DETAIL
+	diff -= abs(diff) * ((GetHR(1,0) + GetHR(-1,0) + GetHR(0,1) + GetHR(0,-1))/4.0 - c0) * acuity;
+#endif
+
+	c0.xyz = c0 + (diff / acuity) * (sqr(m)*grad2*(1 - power) <= power);
 
 	// Dithering
 #ifndef SkipDithering
-	[branch] if (all(p0.xy == size0.xy)) {
+	[branch] if (all(sizeOutput.xy == size0.xy)) {
 		float noise = tempNoise(tex, p0[2]/4);
 		noise = noise * sqrt(12) / acuity;
 		c0.rgb += noise * sqrt(varX*str);
 	}
 #endif
 
+	// Debugging
+	// if (all(p0.xy == size0.xy)) {
+	// 	c0 = round(c0*acuity)/acuity;
+	// }
+	
 	return c0;
 }
