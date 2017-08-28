88a7c5fe59fe57f4155bdf862e815b7e0e9681b2
Shiandow <Shiandow@gmail.com>
2017-08-29 00:10:33 +0200

    SSIMDownscaler: Small tweak, update scalers
    Scalers now handle offset differently.

--- Extensions/RenderScripts/SSimDownscaler/CalcR.hlsl
+++ Extensions/RenderScripts/SSimDownscaler/CalcR.hlsl
@@ -4,6 +4,11 @@ sampler sH:	register(s2);
 
 #define AverageFormat float2x4
 
+#define Kernel(x) pow(0.25, abs(x))
+#define taps 3	
+#define maxtaps taps
+#define Correction (1 - (1 + 4*pow(Kernel(1), 2) + 4 * pow(Kernel(2),2)) / pow(1 + 4*Kernel(1) + 4*Kernel(2), 2))
+
 // -- Define horizontal convolver --
 #define EntryPoint ScaleH
 #define sqr(x)	((x)*(x))
@@ -22,6 +27,7 @@ sampler sH:	register(s2);
 #define EntryPoint 			main
 #define Initialization		float4 mean = tex2D(sMean, tex)
 #define Get(pos) 			ScaleH(pos, mean)
-#define PostProcessing(S)	(S[0] == 0) ? 0 : sqrt(1 + S[1] / S[0])
+#define PostProcessing(S)	(S[0] == 0) ? 0 : sqrt(1 + S[1] * Correction / S[0])
+// #define PostProcessing(S)	(S[0] == 0) ? 0 : sqrt(1 + S[1] / S[0])
 #define axis 1
 #include "./Scalers/Convolver.hlsl"
--- Extensions/RenderScripts/SSimDownscaler/SinglePassConvolver.hlsl
+++ Extensions/RenderScripts/SSimDownscaler/SinglePassConvolver.hlsl
@@ -1,3 +1,7 @@
+#define Kernel(x) pow(0.25, abs(x))
+#define taps 3	
+#define maxtaps taps
+
 // -- Define horizontal convolver --
 #define EntryPoint ScaleH
 #define axis 0
