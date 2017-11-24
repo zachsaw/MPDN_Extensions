a4e9e7b5ed862cbf948e5673b9646a5f898a0689
Shiandow <Shiandow@gmail.com>
2017-11-24 22:51:25 +0100

    [Framework] Implement Filter Compilation
    Reintroduces optimization of filters in a transparent fashion.
    
    Necessary when the exact process can't be determined up front (e.g. the
    source filter).

--- Extensions/Framework/Filter/Filter.cs
+++ Extensions/Framework/Filter/Filter.cs
@@ -39,16 +39,127 @@ namespace Mpdn.Extensions.Framework.Filter
 
     public static class FilterHelper
     {
+        #region Classes
+
+        private class Memoized<TOutput> : ILendable<TOutput>, IDisposable
+        {
+            private readonly Func<ILease<TOutput>> m_Func;
+
+            public Memoized(Func<ILease<TOutput>> func)
+            {
+                m_Func = func;
+            }
+
+            protected virtual void Initialise(TOutput value) { }
+
+            #region Implementation
+
+            private ILease<TOutput> m_Lease;
+
+            public ILease<TOutput> GetLease()
+            {
+                if (m_Lease == null)
+                {
+                    m_Lease = m_Func();
+                    Initialise(m_Lease.Value);
+                }
+                return LeaseHelper.Return(m_Lease.Value);
+            }
+
+            public virtual void Dispose()
+            {
+                DisposeHelper.Dispose(m_Lease);
+            }
+
+            #endregion
+        }
+
+        private class CheckedFilterOutput<TCheck, TDescription, TValue> : Memoized<IFilterBase<IFilterOutput<TDescription, TValue>>>, IFilterOutput<TDescription, TValue>
+            where TCheck : TDescription, IEquatable<TDescription>
+        {
+            private readonly TCheck m_Description;
+
+            public CheckedFilterOutput(TCheck description, ILendable<IFilterBase<IFilterOutput<TDescription, TValue>>> compiler) 
+                : base(compiler.GetLease)
+            {
+                m_Description = description;
+            }
+
+            protected override void Initialise(IFilterBase<IFilterOutput<TDescription, TValue>> value)
+            {
+                base.Initialise(value);
+                if (!m_Description.Equals(value.Output.Description))
+                    throw new InvalidOperationException("Generated output does not match description.");
+            }
+
+            #region IFilterOutput Implementation
+
+            public TDescription Description { get { return m_Description; } }
+
+            ILease<TValue> ILendable<TValue>.GetLease()
+            {
+                return GetLease().Bind(x => x.Output.GetLease());
+            }
+
+            #endregion
+        }
+
+        private class CompiledFilter<TCheck, TDescription, TValue> : CheckedFilterOutput<TCheck, TDescription, TValue>, IFilter<TDescription, TValue>
+            where TCheck : TDescription, IEquatable<TDescription>
+        {
+            public CompiledFilter(TCheck description, ILendable<IFilterBase<IFilterOutput<TDescription, TValue>>> compiler)
+                : base(description, compiler)
+            { }
+
+            #region Implementation
+
+            private IFilterBase<IFilterOutput<TDescription, TValue>> m_FilterBase;
+
+            protected override void Initialise(IFilterBase<IFilterOutput<TDescription, TValue>> value)
+            {
+                base.Initialise(value);
+
+                m_ProcessData.AddInput(value.ProcessData);
+                m_FilterBase = value;
+            }
+
+            #endregion
+
+            #region IFilterBase Implementation
+
+            private readonly IProcessData m_ProcessData = new ProcessData();
+
+            public IProcessData ProcessData { get { return m_ProcessData; } }
+            public TDescription Output { get { return Description; } }
+
+            IFilterOutput<TDescription, TValue> IFilterBase<IFilterOutput<TDescription, TValue>>.Output { get { return this; } }
+
+            public override void Dispose()
+            {
+                base.Dispose();
+                DisposeHelper.Dispose(ref m_FilterBase);
+            }
+
+            #endregion
+        }
+
+        #endregion
+
         public static IFilter<A,X> Return<A,X>(IFilterBase<IFilterOutput<A, X>> filterBase)
         {
             return new Filter<A,X>(filterBase);
         }
 
-        public static IFilter<IEnumerable<A>, IEnumerable<X>> Fold<A, X>(this IEnumerable<IFilter<A, X>> filters)
+        public static IFilter<B, Y> Compile<A, B, Y>(A description, ILendable<IFilterBase<IFilterOutput<B, Y>>> compiler)
+            where A : B, IEquatable<B>
         {
-            return Return(from values in FilterBaseHelper.Fold(filters)
-                          let outputs = filters.Select(x => x.Output)
-                          select FilterOutputHelper.Return(outputs, values.Fold()));
+            return new CompiledFilter<A, B, Y>(description, compiler);
+        }
+
+        public static IFilter<B, Y> Compile<A, B, Y>(A description, Func<IFilterBase<IFilterOutput<B, Y>>> compiler)
+            where A : B, IEquatable<B>
+        {
+            return Compile(description, compiler.MakeDeferred());
         }
 
         public static void Extract<X>(this IFilterBase<ILendable<X>> filter, Action<X> callback)
@@ -59,6 +170,13 @@ namespace Mpdn.Extensions.Framework.Filter
         public static Y Extract<X,Y>(this IFilterBase<ILendable<X>> filter, Func<X,Y> callback)
         {
             return filter.Output.Extract(callback);
+        }
+
+        public static IFilter<IEnumerable<A>, IEnumerable<X>> Fold<A, X>(this IEnumerable<IFilter<A, X>> filters)
+        {
+            return Return(from values in FilterBaseHelper.Fold(filters)
+                          let outputs = filters.Select(x => x.Output)
+                          select FilterOutputHelper.Return(outputs, values.Fold()));
         }
     }
 }
